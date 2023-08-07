c4594df5dac4ac98ae6aa95c4f4f9fb53f7493e2
amivatan <amivatan0011@gmail.com>
2023-08-07 20:54:13 +0330

    adding api and functions for get words

--- learn_english_word.sh
+++ learn_english_word.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
-files=$(ls /usr/share/dict/)
-echo "$files"
-set -x
-for file in $files;
-do
-    echo "$file"
-    lines=$(wc /usr/share/dict/"$file" | awk '{print $1}')
-    if [[ $lines -gt 10000 ]]; then
-        dict=$file
+find_dict() {
+    files=$(ls /usr/share/dict/)
+    set -x
+    for file in $files;
+    do
+        lines=$(wc /usr/share/dict/"$file" | awk '{print $1}')
+        words=$(wc /usr/share/dict/"$file" | awk '{print $2}')
+        if [[ $lines -gt 10000 ]]; then
+            if [[ $lines -eq $words ]];
+            then
+                echo "$file"
+            fi
+        fi
+    done
+    set +x
+}
+get_word_from_1000(){
+    word=$(shuf -n1 ./1-1000.txt)
+    echo "$word"
+}
+get_word() {
+    dict=$(find_dict)
+    word=$(shuf -n1 /usr/share/dict/$dict)
+    echo "$word"
+}
+
+get_definition()
+{
+    word=$(get_word)
+    status_code=$(curl -w "%{http_code}\n" "https://api.dictionaryapi.dev/api/v2/entries/en/$word" | jq | tail -n 1)
+    echo "$status_code"
+    if [[ $status_code == "404" ]];
+    then
+        get_definition
     fi
-done
-set +x
+    data=$(curl -w "%{http_code}\n" "https://api.dictionaryapi.dev/api/v2/entries/en/$word" | jq)
+    echo "$data"
+}
+get_definition
